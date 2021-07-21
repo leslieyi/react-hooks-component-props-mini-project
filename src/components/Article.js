29836f86afee62f267e3f618907feddf3a328138
github username <github email address>
2021-07-21 11:21:48 -0400

    Done with Bonus as well

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,21 @@
-function Article({ title, date ="January 1, 1970", preview }){
- return(
-    
-
-    <article key={title}>
-    <h3>{title}</h3>
-    <small>{date}</small>
-    <p>{preview}</p>
-    </article>
-    
+function Article({title, date="January 1, 1970", preview, minutes}){
+    let emoji
+    let number
+    if (minutes < 30){
+        emoji = '☕️'
+        number = Math.ceil(minutes/5)
+    } else{
+        emoji = '🍱'
+        number = Math.ceil(minutes/10)
+    }
+    return(
+        <article>
+            <h3>{title}</h3>
+            <small>{date}</small>
+            {emoji.repeat(number)} {minutes} min read
+            <p>{preview}</p>
+        </article>
     )
-
 }
 
 export default Article;
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,7 +3,7 @@ import Article from "./Article";
 function ArticleList({ posts }) {
   return (
     <main>
-      {posts.map((post) => (
+      {posts.map(post => (
         <Article
           key={post.id}
           title={post.title}
@@ -14,20 +14,20 @@ function ArticleList({ posts }) {
       ))}
     </main>
   );
-
-  //   What I was doing before.
-  // post.map(post=>{
-  //     <main>
-  //         <Article
-  //           key={post.id}
-  //           title={post.title}
-  //           date={post.date}
-  //           preview={post.preview}
-  //           minutes={post.minutes}
-
-  //     </main>
-  // })
-  // }
 }
+
+//   What I was doing before.
+// post.map(post=>{
+//     <main>
+//         <Article
+//           key={post.id}
+//           title={post.title}
+//           date={post.date}
+//           preview={post.preview}
+//           minutes={post.minutes}
+
+//     </main>
+// })
+// }
 
 export default ArticleList;
